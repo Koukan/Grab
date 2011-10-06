7c86d8dcb7f395e78ef6a9280bb872661ec36246
shotgun <miccerisier@gmail.com>
2011-10-06 20:01:52 +0200

    Update main for chdir in root dir

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include <ClanLib/display.h>
 #include <ClanLib/gl.h>
 #include <ClanLib/network.h>
-#include "Game.hpp"
-
 #include <ClanLib/sound.h>
 #include <ClanLib/vorbis.h>
 #include <ClanLib/mikmod.h>
+#include "Game.hpp"
 #include "AudioManager.hpp"
+
 int	main(int ac, char**av)
 {
+  std::string		path = av[0];
+
+  chdir(path.substr(0, path.rfind('/')).c_str());
   try
   {
     Game::get().exec();
