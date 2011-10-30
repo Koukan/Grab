3bd4d7f646c86c3019c22ea2402ac9f8e1b8127d
shotgun <miccerisier@gmail.com>
2011-10-30 23:40:01 +0100

    Change the default value of layer in GameObjectManager

--- src/Loading.cpp
+++ src/Loading.cpp
@@ -28,9 +28,11 @@ void	Loading::onStart()
 {
   Bullet		*test;
   this->load("resource/intro.xml");
+  this->addGroup("ship", 10);
+  this->addGroup("shot", 5);
   this->addBulletResource("default", "bullet", "shot", "ship", "shot");
   this->addBulletResource("weak", "shot", "shot", "ship", "shot");
-  this->addBulletResource("dummybit", "weapon", "shot", "ship", "shot");
+  this->addBulletResource("dummybit", "weapon", "bullet", "ship", "shot");
   this->addBulletResource("pillarbit", "weapon", "shot", "ship", "shot");
   _parser = new BulletMLParserTinyXML("resource/test.xml");
   _parser->build();
