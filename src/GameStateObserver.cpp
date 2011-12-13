3eb6df9349175c9eac8a1202e4ad04fd1d51abb7
shotgun <miccerisier@gmail.com>
2011-12-13 16:44:04 +0100

    Add GameStateObserver object

---
+++ src/GameStateObserver.cpp
@@ -0,0 +1,17 @@
+#include "GameStateObserver.hpp"
+#include "GameStateManager.hpp"
+
+GameStateObserver::GameStateObserver(std::string const &name) : Module(name)
+{
+	GameStateManager::get().registerObserver(*this);
+}
+
+GameStateObserver::~GameStateObserver()
+{
+}
+
+void			GameStateObserver::notified(Observable<std::list<GameState*> > &source,
+											std::list<GameState*> &arg)
+{
+	this->_glist = arg;
+}
---
+++ src/GameStateObserver.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <list>
+#include "Module.hpp"
+#include "GameState.hpp"
+#include "Observer.hpp"
+#include "Observable.hpp"
+
+class GameStateObserver : public Module, public Observer<std::list<GameState*> >
+{
+public:
+	virtual ~GameStateObserver();
+
+protected:
+	GameStateObserver(std::string const &name);
+	virtual void			notified(Observable<std::list<GameState*> > &source,
+				   					 std::list<GameState*> &arg);
+
+	std::list<GameState*>	_glist;
+};
