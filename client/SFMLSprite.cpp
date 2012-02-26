777d11cd76bc6f65cb48de6fcc2ec32613f85177
castel_a <gianni.castel@gmail.com>
2012-02-26 22:34:29 +0100

    [Client] remove debug

--- client/SFMLSprite.cpp
+++ client/SFMLSprite.cpp
@@ -22,7 +22,6 @@ Core::Resource	*SFMLSprite::clone() const
 
 void		SFMLSprite::update(double elapsedTime)
 {
-	std::cout << this->_frameRate << " " << elapsedTime << std::endl;
 	if (this->_frameRate == -1 || elapsedTime == 0)
 		return ;
 	unsigned int	size = this->_rect.size();
