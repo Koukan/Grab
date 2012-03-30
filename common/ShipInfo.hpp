39e4b2737e7a5b1413b33cf09ddb56a6aae61dbf
castel_a <gianni.castel@gmail.com>
2012-03-31 01:42:30 +0200

    [Common] change ship speed

--- common/ShipInfo.hpp
+++ common/ShipInfo.hpp
@@ -23,15 +23,15 @@ namespace	ShipInfo
 	};
 
 	static ShipInfo const		shipsList[] = {
-  {"Replicator", "player4", "player4", "concentratedPlayer4", 600, 200,
+  {"Replicator", "player4", "player4", "concentratedPlayer4", 500, 200,
    GrabPosition::MIDDLE_TOP, 0, GrabPosition::LEFT_BOTTOM_CORNER, 0, GrabPosition::RIGHT_BOTTOM_CORNER, 0, 0},
-  {"Obliterator", "player3", "player2", "concentratedPlayer2", 600, 200,
+  {"Obliterator", "player3", "player2", "concentratedPlayer2", 500, 200,
    GrabPosition::MIDDLE_BOTTOM, 180, GrabPosition::LEFT_BOTTOM_CORNER, 90, GrabPosition::RIGHT_BOTTOM_CORNER, -90, 0},
-  {"Conqueror", "player1", "player3", "concentratedPlayer3", 600, 400,
+  {"Conqueror", "player1", "player3", "concentratedPlayer3", 500, 400,
    GrabPosition::MIDDLE_TOP, 0, GrabPosition::LEFT_BOTTOM_CORNER, 0, GrabPosition::RIGHT_BOTTOM_CORNER, 0, &Ship::bomb},
-  {"Voyageer", "player2", "player3", "concentratedPlayer3", 600, 800,
+  {"Voyageer", "player2", "player3", "concentratedPlayer3", 500, 800,
    GrabPosition::MIDDLE_BOTTOM, 0, GrabPosition::LEFT_TOP_CORNER, 0, GrabPosition::RIGHT_TOP_CORNER, 0, &Ship::shield},
-  {"LadyBird", "player5", "player3", "concentratedPlayer3", 600, 200,
+  {"LadyBird", "player5", "player3", "concentratedPlayer3", 500, 200,
    GrabPosition::MIDDLE_TOP, 0, GrabPosition::LEFT, 0, GrabPosition::RIGHT, 0, 0},
 	};
 	static unsigned int const	shipsListSize = sizeof(shipsList) / sizeof(*shipsList);
