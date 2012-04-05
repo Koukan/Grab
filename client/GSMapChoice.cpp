ca8216e79ce5779508e969d4f6975baac6fcd037
Shotgun <miccerisier@gmail.com>
2012-04-05 06:09:54 +0200

    [Client] Add MapChoice gamestate

---
+++ client/GSMapChoice.cpp
@@ -0,0 +1,56 @@
+#include "GSMapChoice.hpp"
+#include "RendererManager.hpp"
+#include "GUIVLayout.hpp"
+#include "GUIList.hpp"
+#include "GUIButton.hpp"
+#include "GSPartySettings.hpp"
+
+GSMapChoice::GSMapChoice(Modes::Mode mode)
+	: Core::GameState("mapChoice", true), _mode(mode)
+{
+}
+
+
+GSMapChoice::~GSMapChoice()
+{
+}
+
+void		GSMapChoice::onStart()
+{
+	this->load("resources/xml/intro.xml");
+
+	this->_maps["Map 1"] = "resources/map/map1.xml";
+	this->_maps["Map 2"] = "resources/map/map2.xml";
+
+	Core::GUILayout *layout = new GUIVLayout(VIEWX / 2, (VIEWY - 100) / 2, 300, 300, 0);
+	layout->setY((VIEWY - layout->getHeight()) / 2);
+
+	Core::ButtonSprite *sprite = new Core::ButtonSprite("default button", "selected button", "pressed button");
+	Core::ButtonSprite *leftArrow = new Core::ButtonSprite("left list arrow", "selected left list arrow", "pressed left list arrow");
+	Core::ButtonSprite *rightArrow = new Core::ButtonSprite("right list arrow", "selected right list arrow", "pressed right list arrow");
+
+	GUIList<GSMapChoice> *guilist = new GUIList<GSMapChoice>(*this, &GSMapChoice::mapChoice, *leftArrow, *rightArrow, 0);
+	for (std::map<std::string, std::string>::iterator it = this->_maps.begin();
+		 it != this->_maps.end(); it++)
+		 guilist->addElement(*(new GUIButton<GSMapChoice>(*this, &GSMapChoice::createParty, it->first, "buttonFont", *sprite, 0)));
+	this->_map = this->_maps.begin()->second;
+	layout->insertElementAtEnd(*guilist);
+
+	new GUIButton<GSMapChoice>(*this, &GSMapChoice::createParty, "Next", "buttonFont", *sprite, layout);
+	new GUIButton<GSMapChoice>(*this, &GSMapChoice::back, "Back", "buttonFont", *sprite, layout);
+}
+
+void		GSMapChoice::mapChoice(Core::GUIElement &elem)
+{
+	this->_map = this->_maps[static_cast<GUIButton<GSMapChoice> const &>(elem).getName()];
+}
+
+void		GSMapChoice::createParty()
+{
+	Core::GameStateManager::get().pushState(*new GSPartySettings(this->_mode, this->_map));
+}
+
+void		GSMapChoice::back()
+{
+	Core::GameStateManager::get().popState();
+}
