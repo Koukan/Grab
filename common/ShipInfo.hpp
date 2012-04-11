52d93834064c622e6a29633c0d95d089aa54ea18
Gianni Castellani <gianni.castel@gmail.com>
2012-04-11 21:27:20 +0200

    [Common] add player 3 concentrated fire

--- common/ShipInfo.hpp
+++ common/ShipInfo.hpp
@@ -39,7 +39,7 @@ namespace	ShipInfo
    GrabPosition::MIDDLE_TOP, 0, GrabPosition::LEFT_BOTTOM_CORNER, 0, GrabPosition::RIGHT_BOTTOM_CORNER, 0, SHIELD, 0, -40},
   {"Obliterator", "player3", "player2", "concentratedPlayer2", true, false, 500, 200,
    GrabPosition::MIDDLE_BOTTOM, 180, GrabPosition::LEFT_BOTTOM_CORNER, 90, GrabPosition::RIGHT_BOTTOM_CORNER, -90, BLACKHOLE, 0, 0},
-  {"Conqueror", "player1", "player3", "concentratedPlayer3", true, true, 500, 400,
+  {"Conqueror", "player1", "player3", "concentratedPlayer3", false, false, 500, 400,
    GrabPosition::MIDDLE_TOP, 0, GrabPosition::LEFT_BOTTOM_CORNER, 0, GrabPosition::RIGHT_BOTTOM_CORNER, 0, BOMB, 0, -40},
   {"Voyageer", "player2", "player3", "concentratedPlayer3", true, true, 500, 800,
    GrabPosition::MIDDLE_BOTTOM, 0, GrabPosition::LEFT_TOP_CORNER, 0, GrabPosition::RIGHT_TOP_CORNER, 0, SHIELD, 0, -40},
