dcac9a0fba74efcff96cc12133be1ea12629229b
snap <vbotton@gmail.com>
2012-04-14 19:55:44 +0200

    [Client] fix display error of grab

--- client/GSInGame.cpp
+++ client/GSInGame.cpp
@@ -574,7 +574,7 @@ void		GSInGame::serverCannon(GameCommand const &cmd)
 		if (cmd.data.empty())
 			ship->releaseCannon(cmd.idResource);
 		else
-			ship->addCannon(new Cannon(cmd.data, *ship, *this, "weapon", "cannons", "playerShots", cmd.x, cmd.y), cmd.idResource);
+			ship->addCannon(new Cannon(cmd.data, *ship, *this, "bonus2", "cannons", "playerShots", cmd.x, cmd.y), cmd.idResource);
 	}
 }
 
